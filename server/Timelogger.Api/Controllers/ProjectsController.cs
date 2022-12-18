5315411fccb58659322e1ff8df6450c6df612027
Skyler <skailerknez@gmail.com>
2022-12-17 23:14:15 -0500

    Adding project controllers for inserting new projects and getting the list of project names.

--- server/Timelogger.Api/Controllers/ProjectsController.cs
+++ server/Timelogger.Api/Controllers/ProjectsController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Timelogger.Entities;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Timelogger.Api.Controllers
 {
@@ -25,5 +30,41 @@ namespace Timelogger.Api.Controllers
 		{
 			return Ok(_context.Projects);
 		}
+
+		//filter only names
+		[HttpGet("getNames")]
+		public IActionResult GetAllProjectNames()
+		{
+			List<string> projectNames = _context.Projects.Select(p => p.Name).ToList();
+			return Ok(projectNames);
+		}
+
+		// POST api/projects/addProject
+        [HttpPost("addProject")]
+        public IActionResult AddProject([FromBody] Project newProject)
+        {
+			DateTime todaysDate = DateTime.Now;
+	
+			if(newProject.Deadline < todaysDate){
+				throw new Exception("Project deadline can't be in the past.");
+			}
+
+			if(String.IsNullOrEmpty(newProject.Name)){
+				throw new Exception("Project name can't be empty.");
+			}
+
+			//This is used to replace the database assigned IDs
+			_context.LastProjectId++;
+			newProject.Id = _context.LastProjectId;
+
+			//Seting data for new project
+			newProject.TotalHours = 0;
+			newProject.IsCompleted = false;
+			newProject.Deadline = newProject.Deadline.Date;
+
+			_context.Projects.Add(newProject);
+			_context.SaveChanges();
+            return Ok(_context.Projects);
+        }
 	}
 }
